2e89522bee739f683d2fd8957760374bc6b5c3ba
acboileau-lgtm <ac.boileau@gmail.com>
2026-08-13 17:07:50 +0200

    fix: autoriser Mijote en local via CORS

--- server.js
+++ server.js
@@ -13,10 +13,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
 
-    res.header(
-        "Access-Control-Allow-Origin",
-        "http://127.0.0.1:5500"
-    );
+    const allowedOrigins = [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ];
+
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header(
+            "Access-Control-Allow-Origin",
+            origin
+        );
+    }
 
     res.header(
         "Access-Control-Allow-Methods",
